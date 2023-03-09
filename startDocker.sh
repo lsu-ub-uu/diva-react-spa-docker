b7cf9d38606847b1a6738d5d7fbe015cea3832d7
egiiiiil <sixteenth.bird@gmail.com>
2023-03-09 09:36:00 +0100

    init

--- startDocker.sh
+++ startDocker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker kill diva-react-spa-dev && echo nothingToSeeMoveOnToNextCommand
 
+docker rm diva-react-spa-dev && echo nothingToSeeMoveOnToNextCommand
+
 docker run --rm -d\
     --name diva-react-spa-dev \
     -p 9876:80 \
