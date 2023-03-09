78dc4f8fa3a75afb998c19706d3578d0f56d497a
egiiiiil <sixteenth.bird@gmail.com>
2023-03-09 09:06:11 +0100

    init

--- startDocker.sh
+++ startDocker.sh
@@ -3,7 +3,7 @@ docker kill diva-react-spa-dev && echo nothingToSeeMoveOnToNextCommand
 
 docker rm diva-react-spa-dev && echo nothingToSeeMoveOnToNextCommand
 
-docker run --rm -d\
-    --name diva-react-spa-dev \
+docker run --name diva-react-spa-dev \
+    --restart unless-stopped -d \
     -p 9876:80 \
     diva-react-spa-docker:latest $1
